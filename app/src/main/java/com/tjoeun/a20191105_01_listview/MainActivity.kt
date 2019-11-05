252d9c023c2fbf27edb5665c94a98bfda20a44da
sangone88 <llil1@naver.com>
2019-11-05 21:14:08 +0900

    만든 어댑터를 메인 액티비티에서 활용  - roomDataAdapter를 멤버 변수로 생성.  - onCreate에서 기본생성자를 이용해서 객체화.    => this, R.layout.리스트아이템, 객체를가진list(roomList)  - 리스트뷰를 코틀린에서 어댑터를 붙쳐줌,    => roomListView.adapter = roomDataAdapter

--- app/src/main/java/com/tjoeun/a20191105_01_listview/MainActivity.kt
+++ app/src/main/java/com/tjoeun/a20191105_01_listview/MainActivity.kt
@@ -2,17 +2,24 @@ package com.tjoeun.a20191105_01_listview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.tjoeun.a20191105_01_listview.adapters.RoomDataAdapter
 import com.tjoeun.a20191105_01_listview.datas.RoomData
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     var roomList = ArrayList<RoomData>()
+    var roomAdapter:RoomDataAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         addRooms()
+
+        roomAdapter = RoomDataAdapter(this, R.layout.room_list_item, roomList)
+
+        roomListView.adapter = roomAdapter
 
     }
 
